243431f8f3c6d5a85fa84993b7d4854041b2de2f
YisRime <118952786+YisRime@users.noreply.github.com>
2025-06-10 11:16:50 +0800

    feat: 添加 giscus 评论插件支持，更新主题设置

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,36 @@
-// https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
 
 export default {
   extends: DefaultTheme,
+
+  setup() {
+    const { frontmatter } = useData();
+    const route = useRoute();
+
+    giscusTalk({
+      repo: 'GuideMC/GuideMC.github.io',
+      repoId: 'R_kgDONVGrAQ',
+      category: 'Announcements',
+      categoryId: 'DIC_kwDONVGrAc4Ckohc',
+      mapping: 'pathname',
+      reactionsEnabled: '1',
+      inputPosition: 'top',
+      theme: 'preferred_color_scheme',
+      lang: 'zh-CN',
+      loading: 'lazy'
+      }, 
+      {
+        frontmatter, route
+      },
+      true
+    );
+  },
+
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
